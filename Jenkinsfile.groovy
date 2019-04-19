077a47c5e3fa971859e0568bc96f8210c218f11a
unknown <panamavr@hotmail.com>
2019-04-19 12:30:14 +0100

    fixing jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -18,6 +18,11 @@ pipeline {
                     }
                 }
         }
+        stage('Deploy to staging') {
+            steps {
+                build job: 'deploy-to-staging'
+            }
+        }
 
     }
 }
