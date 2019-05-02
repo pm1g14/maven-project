63361e66079186422070a716f8d639412a18f3f8
unknown <panamavr@hotmail.com>
2019-05-02 23:38:53 +0100

    adding dockerfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,37 +1,12 @@
 pipeline {
     agent any
 
-    parameters {
-        string(name: 'tomcat_staging', defaultValue: 'localhost', description: 'some desc')
-    }
-
-    triggers {
-        pollSCM('* * * * *')
-    }
-
-    tools {
-        maven 'maven'
-    }
     stages {
-
         stage('Build') {
             steps {
                 bat 'mvn clean package'
-            
-            }
-             post {
-                    success {
-                        echo 'Now archiving...'
-                        archiveArtifacts artifacts: '**/target/*.war'
-                    }
-                }
-        }
-        stage('Deploy to staging') {
-            steps {
-            //    build job: 'deploy-to-staging'
-                bat "winscp **/target/*.war ${params.tomcat_staging}:C:/Program Files/Apache Software Foundation/Tomcat 7.0/webapps"
+                bat "docker build . -t tomcatwebapp:${env.BUILD_ID}"
             }
         }
-
     }
 }
